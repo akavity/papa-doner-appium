4d624fe6b00e2b9e11efbac21e2c26acbae32a40
Basil Akavity <vakavity@gmail.com>
2024-06-29 00:07:11 +0300

    added DataProvider, models, annotation TestData, changed DonerTest

---
+++ src/main/java/org/example/models/donerTest/StartData.java
@@ -0,0 +1,14 @@
+package org.example.models.donerTest;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class StartData {
+    String deliveryType;
+
+    String cafeAddress;
+}
--- src/test/java/DonerTest.java
+++ src/test/java/DonerTest.java
@@ -1,18 +1,21 @@
-import org.example.pages.MenuPage;
+import org.example.annotations.TestData;
+import org.example.models.donerTest.StartData;
 import org.example.steps.MenuSteps;
 import org.example.steps.StartScreenSteps;
+import org.example.utils.JsonReader;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class DonerTest extends BaseTest {
     MenuSteps menu = new MenuSteps();
     StartScreenSteps start = new StartScreenSteps();
-    MenuPage menuPage = new MenuPage();
 
-    @Test
-    public void selectPickUpDeliveryTest() {
-        start.selectDeliveryType("Самовывоз");
-        start.selectCafe("ст. м. Каменная Горка");
+    @TestData(jsonFile = "startData", model = "StartData", folder = "donerTest")
+    @Test(description = "Select address of cafe",
+            dataProviderClass = JsonReader.class, dataProvider = "getData")
+    public void selectPickUpDeliveryTest(StartData startData) {
+        start.selectDeliveryType(startData.getDeliveryType());
+        start.selectCafe(startData.getCafeAddress());
         start.clickSubmitButton();
         start.clickAllowButton();
 
